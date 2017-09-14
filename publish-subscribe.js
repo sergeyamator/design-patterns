cb1f519b801c67c3f117a37b0c5d25d0c8a5e53f
Serhii Rubets <serhii_rubets@epam.com>
2017-09-14 15:04:03 +0300

    Simple subscribe/publisher was added

---
+++ publish-subscribe.js
@@ -0,0 +1,23 @@
+const EventBas = {
+    channels: {},
+    subscribe(channelName, listener) {
+        if (!this.channels[channelName]) {
+            this.channels[channelName] = [];
+        }
+        this.channels[channelName].push(listener)
+    },
+    publish(channelName, data) {
+        const channel = this.channels[channelName];
+        if (!channel || !channel.length) {
+            return;
+        }
+
+        channel.forEach(listener => listener(data));
+    }
+};
+
+EventBas.subscribe('foo', (msg) => {
+    console.log(msg);
+});
+
+EventBas.publish('foo', 'Hello world');
